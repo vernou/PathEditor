86ab0f62b6b11f4463b40793178fc70582e33344
Cedric VERNOU <cedric.vernou@gmail.com>
2018-09-23 22:15:30 +0200

    #31 Display Process Environment Variable Path

---
+++ src/PathEditor.Core/EnvironmentVariablePath/FormattedProcessEnvironmentVariablePath.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace PathEditor.Core.EnvironmentVariablePath
+{
+    public sealed class FormattedProcessEnvironmentVariablePath
+    {
+        private readonly IFormattedEnvironmentVariablePath _formatted;
+
+        public FormattedProcessEnvironmentVariablePath(IFormattedEnvironmentVariablePath formattedEnvironmentVariablePath)
+        {
+            _formatted = formattedEnvironmentVariablePath;
+        }
+
+        public string Value => Compute();
+
+        private string Compute()
+        {
+            if (_formatted.System == string.Empty)
+                return _formatted.User;
+            return _formatted.System + (_formatted.System.Last() == ';' ? string.Empty : ";") + Environment.NewLine + _formatted.User;
+        }
+    }
+}
---
+++ src/PathEditor.UnitTests/EnvironmentVariablePath/FormattedProcessEnvironmentVariablePathTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PathEditor.Core.EnvironmentVariablePath;
+using Xunit;
+
+namespace PathEditor.UnitTests.EnvironmentVariablePath
+{
+    public sealed class FormattedProcessEnvironmentVariablePathTest
+    {
+        public static IEnumerable<object[]> ValueData()
+        {
+            var nl = Environment.NewLine;
+            yield return new object[] { string.Empty, string.Empty, string.Empty };
+            yield return new object[]
+            {
+                string.Empty,
+                $@"C:\User\Path1;{nl}C:\User\Path2;{nl}C:\User\Path3",
+                $@"C:\User\Path1;{nl}C:\User\Path2;{nl}C:\User\Path3",
+            };
+            yield return new object[]
+            {
+                $@"C:\System\Path1;{nl}C:\System\Path2;{nl}C:\System\Path3",
+                $@"C:\User\Path1;{nl}C:\User\Path2;{nl}C:\User\Path3",
+                $@"C:\System\Path1;{nl}C:\System\Path2;{nl}C:\System\Path3;{nl}C:\User\Path1;{nl}C:\User\Path2;{nl}C:\User\Path3"
+            };
+            yield return new object[]
+            {
+                $@"C:\System\Path;{nl}C:\System\Path2;{nl}C:\System\Path3;",
+                $@"C:\User\Path1;{nl}C:\User\Path2;{nl}C:\User\Path3;",
+                $@"C:\System\Path;{nl}C:\System\Path2;{nl}C:\System\Path3;{nl}C:\User\Path1;{nl}C:\User\Path2;{nl}C:\User\Path3;"
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(ValueData))]
+        public void Value(string formattedSystem, string formattedUser, string expectedFormattedProcess)
+        {
+            var nl = Environment.NewLine;
+            Assert.Equal(expectedFormattedProcess,
+                new FormattedProcessEnvironmentVariablePath(
+                    new FormattedEnvironmentVariablePathFake(
+                        formattedSystem,
+                        formattedUser)
+                ).Value
+            );
+        }
+    }
+}
